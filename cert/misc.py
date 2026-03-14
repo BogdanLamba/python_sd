88552a153a2c92ee1b31ca19f987a4625035e4f7
BogdanLamba <bogdan.alex.lamba@live.com>
2026-03-14 11:30:25 +0200

    # added module 8 doc

--- cert/misc.py
+++ cert/misc.py
@@ -1,19 +1,17 @@
-suma = 0
-for i in range(1, 101):
-    suma += i
-print(f"Suma numerelor de la 1 la 100 este {suma}")
+numere = []
+for i in range(5):
+    numere.append(int(input(f"Numărul {i + 1}: ")))
 
-i = 1
-while i <= 1000:
-    if i % 7 == 0 and i % 11 == 0:
-        print(f"Primul numar din intervalul [1, 1000] care este divizibil cu 7 și cu 11 simultan este {i}")
-        break
-    i += 1
+print(f"Suma numerelor din lista este {sum(numere)}")
+print(f"Media numerelor din lista este {sum(numere) / len(numere):.2f}")
+print(f"Minim din lista este {min(numere)}")
+print(f"Maximum din lista este {max(numere)}")
 
-number = 97
-for i in range(2, number):
-    if number % i == 0:
-        print(f"Numarul nu este prim, este divizibil cu: {i}!")
-        break
-else:
-    print(f"Numarul {number} este prim!")
+print(f"Lista sortata crescator este {sorted(numere)}")
+print(f"Lista sortata descrescator este {sorted(numere, reverse=True)}")
+
+media = sum(numere) / len(numere)
+for i in range(len(numere) - 1, -1, -1):
+    if numere[i] < media:
+        del numere[i]
+print(f"Lista dupa stergerea elementelor sub medie: {numere}")
